88c77f49a99d243ac7f633ce4925ce120bd10047
Shahnawaz Khan <nawazkhan654321@gmail.com>
2020-12-14 13:51:26 +0530

    chore: refactored annotator to add prop types and comments

--- src/components/Annotator.js
+++ src/components/Annotator.js
@@ -2,22 +2,18 @@ import React from 'react'
 // import Annotator from '@rediminds/image-video-annotator'
 import Annotator from '../Annotator/src/Annotator'; 
 import { images, labels } from './images'
-import { Route,useHistory,useParams } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 const AppAnnotator = (props) => {
     const history = useHistory();
-    const params = useParams();
-    // console.log("Props:", params);
-
     return (
-        // <Route path={`/:job/:asset/`} exact >
-
-        <div>
-            <Annotator images={images}
+        <Annotator
+            images={images}
                 regionClsList={labels}
-                jobName="Dr-John-De"
+                jobName="Dr-John-De"            
                 renderError={(error) => {
                     // Message.error(error)
                 }}
+
                 useHistory={()=> history}
                 onSave={(selectedImage, encodedData) => {
                     // console.log('Encoded data', encodedData);
@@ -27,8 +23,6 @@ const AppAnnotator = (props) => {
                 onSubmit={(state) => {
                     
                 }} />
-            </div>
-        // </Route>
             
     )
 }

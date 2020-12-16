0e2d5bfd13da0df958db99625394cafa7d937aab
Sunny Kumar <sunny.kumar@appwrk.com>
2020-12-16 14:17:52 +0530

    ADDED: Added routes in app.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import './App.css';
 import Annotator from './components/Annotator';
 import React from 'react'
-import { BrowserRouter as Router } from 'react-router-dom';
-function App(props) {
-  
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+function App(props) {
   return (
     <Router>
-      <Annotator />
+      <Route path="/annotate/:job/:asset" component={Annotator} />
     </Router>
-
   );
 }
 
 export default App;
-
-
-
--- src/components/Annotator.js
+++ src/components/Annotator.js
@@ -2,34 +2,22 @@ import React from 'react'
 // import Annotator from '@rediminds/image-video-annotator'
 import Annotator from '../Annotator/src/Annotator'; 
 import { images, labels } from './images'
-import { Route,useHistory,useParams } from 'react-router-dom';
+import { Route, useHistory, useParams } from 'react-router-dom';
+
 const AppAnnotator = (props) => {
     const history = useHistory();
-    const params = useParams();
-    // console.log("Props:", params);
 
     return (
-        // <Route path={`/:job/:asset/`} exact >
-
         <div>
             <Annotator images={images}
                 regionClsList={labels}
                 jobName="Dr-John-De"
-                renderError={(error) => {
-                    // Message.error(error)
-                }}
+                renderError={(error) => {}}
                 useHistory={()=> history}
-                onSave={(selectedImage, encodedData) => {
-                    // console.log('Encoded data', encodedData);
-                    // alert('Changes saved successfully.')
-
-                }}
-                onSubmit={(state) => {
-                    
-                }} />
-            </div>
-        // </Route>
-            
+                onSave={(selectedImage, encodedData) => {}}
+                onSubmit={(state) => {}} 
+            />
+        </div>
     )
 }
 
